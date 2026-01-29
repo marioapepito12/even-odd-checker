284189d0c4bb95b84c0f6bf61c4c11efef60307c
marioapepito12 <marioapepito12@gmail.com>
2026-01-30 00:29:53 +0800

    updated even_odd.cpp

--- even_odd.cpp
+++ even_odd.cpp
@@ -3,15 +3,23 @@ using namespace std;
 
 int main() {
     int number;
+    char choice;
 
-    cout << "Enter a number: ";
-    cin >> number;
+    do {
+        cout << "Enter a number: ";
+        cin >> number;
 
-    if (number % 2 == 0) {
-        cout << "The number is even." << endl;
-    } else {
-        cout << "The number is odd." << endl;
-    }
+        if (number % 2 == 0) {
+            cout << number << " is an even number." << endl;
+        } else {
+            cout << number << " is an odd number." << endl;
+        }
 
+        cout << "Check another number? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "Program ended." << endl;
     return 0;
 }
